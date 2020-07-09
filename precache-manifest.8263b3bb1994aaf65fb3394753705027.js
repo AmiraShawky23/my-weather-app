39643d60c8029f5d1932ae4563e1836e3a345b47
SAmira <amirashawkym23@gmail.com>
2020-07-10 00:20:23 +0200

    Updates

---
+++ precache-manifest.8263b3bb1994aaf65fb3394753705027.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05e4c0fd30a798ac1460129f126b8795",
+    "url": "/my-weather-app/index.html"
+  },
+  {
+    "revision": "7597ce0bba7bcafe8450",
+    "url": "/my-weather-app/static/css/main.23d725f0.chunk.css"
+  },
+  {
+    "revision": "12d13d93fd9fce08fb12",
+    "url": "/my-weather-app/static/js/2.9f55b04b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my-weather-app/static/js/2.9f55b04b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7597ce0bba7bcafe8450",
+    "url": "/my-weather-app/static/js/main.126bb358.chunk.js"
+  },
+  {
+    "revision": "15e17cd02a1ea66719a8",
+    "url": "/my-weather-app/static/js/runtime-main.c7bbd4fa.js"
+  },
+  {
+    "revision": "2b7c48a767e22546941d70afc9f27ff0",
+    "url": "/my-weather-app/static/media/map-marker-alt-solid.2b7c48a7.svg"
+  },
+  {
+    "revision": "c412776699c042548bfc30e7b2fa85ca",
+    "url": "/my-weather-app/static/media/search.c4127766.svg"
+  }
+]);
